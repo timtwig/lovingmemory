4b6079b354192b6e0411d42076e33ef7b5d74691
timtwig <mail@tim-Atkins.co.uk>
2021-03-25 19:33:35 +0000

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,16 +5,11 @@
     </div>
     <div class="section details">
       <h1 class="title">Charity Donations</h1>
-      <p>Instead of flowers we would like to suggest people make a donation to one of the following charities</p>
-      <h2>Marie Curie</h2>
-      <p>We are so grateful to <em>Marie Curie</em> for their support and making sure their patients are as 
-        comfortable and peaceful as possible when the end came.
+      <p>Instead of flowers we would like to suggest people make a donation to Compassion UK, a charity very close to Sylvia's heart</p>
+      <h2>Compassion UK</h2>
+      <p>Sylvia loved to sponsor children through the Christian charity <em>Compassion UK</em> and we would appreciate your ongoing support for their fantastic work in helping children all over the world.
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to Marie Curie</a>
-      <h2>Make a Wish</h2>
-      <p><em>Make A Wish</em>'s goal of giving joy to children during a terrible time is something that would be a fitting tribute.</p>
-      <a class="button" target="_blank" rel=”noopener” href="https://www.justgiving.com/fundraising/">Donate to Make a Wish</a>
-    </div>
+
   </div>
 </template>
 
