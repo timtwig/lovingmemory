d93f5bec89154f59f82181c472551533c47c66bb
timtwig <mail@tim-Atkins.co.uk>
2021-03-25 19:23:05 +0000

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -7,12 +7,12 @@
       <h1 class="title">Service details</h1>
       <p><em>Tuesday 6th April 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Sylvias life later in the year when COVID-19 restrictions are relaxed.</p>
       <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
-            <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Church Service</h2>
-      <p>The church service is being performed at Church address
+            <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually.</p>
+      <h2>Church Service - Livestream</h2>
+      <p>The church service is being performed at Bromford Bridge Christian Centre
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
-      <h2>Church Service <span>12pm</span></h2>
+      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12pm</a>
+      <h2>Church Service - Order <span>12pm</span></h2>
       <ul class="fa-ul">
         <ListItem>Service starts at 12pm at Bromford Bridge Christian Centre</ListItem>
         <ListItem>Entrance music: Jesu Joy of Man's desire Be</ListItem>
@@ -21,23 +21,12 @@
         <ListItem>Song: Goodness of God</ListItem>
         <ListItem>Tributes - Family &amp; Friends</ListItem>
         <ListItem>Few moments of quiet</ListItem>
-        <ListItem>Short message</ListItem>
+        <ListItem>Tribute - Brian Atkins</ListItem>
         <ListItem>Prayer of Commendation</ListItem>
         <ListItem>Closing prayer &amp; Grace blessing</ListItem>
-        <ListItem>Exit music: New Wine</ListItem>
+        <ListItem>Exit music: Waymaker</ListItem>
       </ul>
       <h2>Burial <span>1.30pm</span></h2>
-      <ul class="fa-ul">
-        <ListItem>Service starts at 12pm at Crematorium address</ListItem>
-        <ListItem>Entrance music: And Can It Be</ListItem>
-        <ListItem>Scripture reading - Psalm 61 v1-4</ListItem>
-        <ListItem>Welcome &amp; Opening Prayer</ListItem>
-        <ListItem>Tribute</ListItem>
-        <ListItem>Few moments of quiet</ListItem>
-        <ListItem>Short message</ListItem>
-        <ListItem>Committal</ListItem>
-        <ListItem>Closing prayer &amp; Grace blessing</ListItem>
-        <ListItem>Exit music: New Wine</ListItem>
       </ul>
     </div>
   </div>
