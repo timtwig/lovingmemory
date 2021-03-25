71b5399dca0ee3366191c7c54779718707305f2c
timtwig <mail@tim-Atkins.co.uk>
2021-03-25 18:57:42 +0000

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,12 +5,12 @@
     </div>
     <div class="section details">
       <h1 class="title">Order of service</h1>
-      <p><em>Thursday 28th January 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Lyns life later in the year when COVID-19 restrictions are relaxed.</p>
+      <p><em>Tuesday 6th April 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Sylvias life later in the year when COVID-19 restrictions are relaxed.</p>
       <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
-      <h2>Church Service <span>10:30am</span></h2>
+      <h2>Church Service <span>12pm</span></h2>
       <ul class="fa-ul">
-        <ListItem>Service starts at 10:30am at Church Address</ListItem>
-        <ListItem>Entrance music: And Can It Be</ListItem>
+        <ListItem>Service starts at 12pm at Bromford Bridge Christian Centre</ListItem>
+        <ListItem>Entrance music: Jesu Joy of Man's desire Be</ListItem>
         <ListItem>Scripture reading</ListItem>
         <ListItem>Welcome &amp; Opening Prayer</ListItem>
         <ListItem>Song: Goodness of God</ListItem>
@@ -21,7 +21,7 @@
         <ListItem>Closing prayer &amp; Grace blessing</ListItem>
         <ListItem>Exit music: New Wine</ListItem>
       </ul>
-      <h2>Crematorium <span>12:00pm</span></h2>
+      <h2>Burial <span>1.30pmpm</span></h2>
       <ul class="fa-ul">
         <ListItem>Service starts at 12pm at Crematorium address</ListItem>
         <ListItem>Entrance music: And Can It Be</ListItem>
