1f406080631861e28982b81f5021a433ac38a0e0
timtwig <mail@tim-Atkins.co.uk>
2021-03-25 19:27:16 +0000

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -5,17 +5,9 @@
     </div>
     <div class="section details">
       <h1 class="title">Your tributes</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Church Service</h2>
-      <p>The church service is being performed at Church address
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
-      <h2>Crematorium</h2>
-      <p>The service is being held at Crematorium address</p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
-      <ul class="fa-ul">
-        <ListItem>Username: <span class="highlight">test123</span> <CopyButton text="test123"></CopyButton></ListItem>
-        <ListItem>Password: <span class="highlight">password123</span> <CopyButton text="password123"></CopyButton></ListItem>
+
+We would love for you to take some time and leave a tribute or share some of your own memories of Sylvia. To do so, please click on the following link:
+https://beyond.life/obituaries/sylvia-atkins-12-03-2021
       </ul>
 
     </div>
