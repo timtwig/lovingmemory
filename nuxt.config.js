55a68a35b9aeca866218f7a03628ad2d8df22cbc
timtwig <mail@tim-Atkins.co.uk>
2021-03-25 17:27:59 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
-const name = 'Janet Doe'
-const dates = 'July 29th 1958 - Jan 4th 2021'
+const name = 'Sylvia Atkins'
+const dates = '23rd March 1944 - 12th March 2021'
 
 export default {
   publicRuntimeConfig: {
