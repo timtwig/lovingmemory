a1fe93c5723260eda6376427a2f12f84f99fb1d1
timtwig <mail@tim-Atkins.co.uk>
2021-03-26 19:05:13 +0000

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -5,9 +5,9 @@
     </div>
     <div class="section details">
       <h1 class="title">Charity Donations</h1>
-      <p>Instead of flowers we would like to suggest people make a donation to Compassion UK, a charity very close to Sylvia's heart</p>
+      <p>In lieu of flowers, we would like to request that donations (if desired) be made to charity.</p>
       <h2>Compassion UK</h2>
-      <p>Sylvia loved to sponsor children through the Christian charity <em>Compassion UK</em> and we would appreciate your ongoing support for their fantastic work in helping children all over the world.
+      <p>Sylvia loved children. From becoming a Sunday School Teacher at the age of 12 to running the tuck shop at JaM Kids Club right up until lockdown last March, Sylvia committed much of her life to children. As well as raising her own family, Sylvia helped children to read at the local Primary School, volunteered to support swimming lessons, ran community toddler groups and netball clubs, and was in her element at Summer Camps! Sylvia was an advocate and dedicated sponsor of less fortunate children around the world through Compassion UK; their objective of providing an education and opportunity for these children would be a fitting tribute to Sylvia's memory.
 
       </p>
       <a class="button" target="_blank" rel=”noopener” href="https://abtaylorfunerals.co.uk/donations/">Donate here</a>
