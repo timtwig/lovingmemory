274b839f3208e8de055250c29235be2c11cb6ea7
timtwig <mail@tim-Atkins.co.uk>
2021-03-26 17:21:22 +0000

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -8,6 +8,7 @@
       <p>Instead of flowers we would like to suggest people make a donation to Compassion UK, a charity very close to Sylvia's heart</p>
       <h2>Compassion UK</h2>
       <p>Sylvia loved to sponsor children through the Christian charity <em>Compassion UK</em> and we would appreciate your ongoing support for their fantastic work in helping children all over the world.
+      <a class="button" target="_blank" rel=”noopener” href="">Donate here</a>
       </p>
 
   </div>
