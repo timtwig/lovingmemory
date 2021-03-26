5ad6750e458622dbb6cd5fe746ae0831a7563504
timtwig <mail@tim-Atkins.co.uk>
2021-03-26 19:18:24 +0000

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,14 +5,14 @@
     </div>
     <div class="section details">
       <h1 class="title">Service details</h1>
-      <p><em>Tuesday 6th April 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Sylvias life later in the year when COVID-19 restrictions are relaxed.</p>
-      <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
+      <p><em>Tuesday 6th April 2021</em> 
+      Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Sylvia's life later in the year when COVID-19 restrictions are relaxed.</p>
             <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually.</p>
       <h2>Church Service - Livestream</h2>
       <p>The church service is being performed at Bromford Bridge Christian Centre
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12pm</a>
-      <h2>Church Service - Order <span>12pm</span></h2>
+      <a class="button" target="_blank" rel=”noopener” href="">Click here for the Livestream - 12pm</a>
+      <h2>Church Service - Order - 12pm</h2>
       <ul class="fa-ul">
         <ListItem>Service starts at 12pm at Bromford Bridge Christian Centre</ListItem>
         <ListItem>Entrance music: Jesu Joy of Man's desire Be</ListItem>
@@ -26,7 +26,7 @@
         <ListItem>Closing prayer &amp; Grace blessing</ListItem>
         <ListItem>Exit music: Waymaker</ListItem>
       </ul>
-      <h2>Burial <span>1.30pm</span></h2>
+      <h2>Burial - 1.30pm</h2>
       </ul>
     </div>
   </div>
