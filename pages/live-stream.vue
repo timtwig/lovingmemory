74f91ee465bba6d80a5140aaf21c990615921f72
timtwig <mail@tim-Atkins.co.uk>
2021-03-27 13:12:25 +0000

    Update live-stream.vue

--- pages/live-stream.vue
+++ pages/live-stream.vue
@@ -6,7 +6,7 @@
     <div class="section details">
       <h1 class="title">Your tributes</h1>
 <p>
-We would love for you to take some time and leave a tribute or share some of your own memories of Sylvia. 
+We would love for you to take some time and leave a tribute or share some of your own memories of Sylvia. These will be collated into a book in the near future.
 </p>
 <a class="button" target="_blank" rel=”noopener” href="https://beyond.life/obituaries/sylvia-atkins-12-03-2021">Leave your tributes for Sylvia here</a>
 
