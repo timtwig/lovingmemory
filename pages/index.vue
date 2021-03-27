466d78d50a91d8cf2da83f1222eb60931063740b
timtwig <mail@tim-Atkins.co.uk>
2021-03-27 12:21:13 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,12 @@
     <div class="section tribute">
       <h1 class="title">In Loving Memory</h1>
       <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
-      <img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" /><img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" />
+      <table>
+      <tr>
+      <td><img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" /></td>
+      <td><img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" /></td>
+      </tr>
+      </table>
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
     </div>
   </div>
