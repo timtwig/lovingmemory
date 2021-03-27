9df82047694df534971e4fed36057e4ca8e3d04d
timtwig <mail@tim-Atkins.co.uk>
2021-03-27 14:20:45 +0000

    Update charity-donation.vue

--- pages/charity-donation.vue
+++ pages/charity-donation.vue
@@ -8,7 +8,7 @@
       <p>In lieu of flowers, we would like to request that donations (if desired) be made to charity.</p>
       <h2>Compassion UK</h2>
       <p>
-      Sylvia loved children. From becoming a Sunday School Teacher at the age of 12 to running the tuck shop at JaM Kids Club right up until lockdown last March, 
+      Sylvia loved children. From becoming a Sunday School Teacher at the age of 13 to running the tuck shop at JaM Kids Club right up until lockdown last March, 
       Sylvia committed much of her life to children. As well as raising her own family, Sylvia helped children to read at the local Primary School, 
       volunteered to support swimming lessons, ran community toddler groups and netball clubs, and was in her element at Summer Camps! 
       Sylvia was an advocate and dedicated sponsor of less fortunate children around the world through Compassion UK; their objective of providing 
