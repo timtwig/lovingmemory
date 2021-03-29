464d835d9900183af4a54c89fb1c4f45a0a3b2ff
timtwig <mail@tim-Atkins.co.uk>
2021-03-29 16:58:13 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,9 @@
       <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
       <table>
       <tr>
-      <td><img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" /></td>
-      <td><img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" /></td>
-      <td><img height="60" class="top" src="~/assets/primrose.png" alt="Primrose" /></td>
+      <td><img height="60" class="top" src="~/assets/primrose1.png" alt="Primrose" /></td>
+      <td><img height="60" class="top" src="~/assets/primrose1.png" alt="Primrose" /></td>
+      <td><img height="60" class="top" src="~/assets/primrose1.png" alt="Primrose" /></td>
       </tr>
       </table>
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
