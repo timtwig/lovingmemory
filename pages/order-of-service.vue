fa2ca6aeb581b7a27f01d2c263cedd71db6e21de
timtwig <mail@tim-Atkins.co.uk>
2021-03-31 19:49:52 +0100

    Add livestream embedded

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -10,8 +10,7 @@
       <h2>Church Service - Livestream</h2>
       <p>The church service is being performed at Bromford Bridge Christian Centre
       </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Click here for the Livestream</a>
-      </ul>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/_davN-ic8I0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
 </template>
