ce83475cd0e3f9d2bbbdf144ffb4f18691883f84
timtwig <mail@tim-Atkins.co.uk>
2021-03-31 18:35:40 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section details">
       <h1 class="title">Service details</h1>
-      <p><em>Tuesday 6th April 2021</em></p>
+      <p><em>Tuesday 6th April 2021 at 12pm</em></p>
       Due to current restrictions, we are planning to live stream the service so everyone who wants to join us will be able to do so virtually. We also hope to hold a celebration of Sylvia's life later in the year, circumstances permitting.</p>
       <h2>Church Service - Livestream</h2>
       <p>The church service is being performed at Bromford Bridge Christian Centre
