fd176497a85509e295290a74b614407da19e615b
timtwig <mail@tim-Atkins.co.uk>
2021-04-01 14:37:24 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -15,6 +15,23 @@
       </div>
     <p> If you are experiencing any difiiculties with the above link, please <a target="_blank" href="https://www.youtube.com/embed/_davN-ic8I0">click here</a> to watch directly on Youtube.</p>
     </div>
+          <h2>Church Service - Order - 12pm</h2>
+      <ul class="fa-ul">
+        <ListItem>Service starts at 12pm at Bromford Bridge Christian Centre</ListItem>
+        <ListItem>Entrance music: Jesu Joy of Man's desire – played by Brian Atkins</ListItem>
+        <ListItem>Welcome &amp; Opening Prayer – Matthew Atkins</ListItem>
+        <ListItem>Song: <a href=”https://www.wearegateway.ca/it/downloads/music/lyrics/10,000Reasons.pdf” target=”_blank”>10,000 Reasons</a></ListItem>
+        <ListItem>Bible reading – Psalm 116:1-7 – read by Grace Atkins</ListItem>
+        <ListItem>Tribute - Brian Atkins</ListItem>
+        <ListItem>Hymn: <a href=”https://library.timelesstruths.org/music/There_Is_Power_in_the_Blood/” target=”_blank”>Power in the Blood</a></ListItem>
+        <ListItem>Prayer</ListItem>
+        <ListItem>Tribute from Children – shared by Nathan Atkins</ListItem>
+        <ListItem>Song: <a href=”https://district.church/wp-content/uploads/Way-Maker-Leeland-Lyrics.pdf” target=”_blank”>Way maker</a></ListItem>
+        <ListItem>Bible reading – John 12: 24-26 – read by Toby Atkins</ListItem>
+        <ListItem>Tribute - Grandchildren</ListItem>
+        <ListItem>Prayer of Commendation</ListItem>
+        <ListItem>Exit music: <a href=”https://www.wearegateway.ca/it/downloads/music/lyrics/NoLongerSlaves.pdf” target=”_blank”>Child of God</ListItem>
+      </ul>
   </div>
 </template>
 
