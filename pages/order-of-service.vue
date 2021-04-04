8c6570917d6e871144aa45253541879bf19d4f4f
timtwig <mail@tim-Atkins.co.uk>
2021-04-04 20:55:31 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -13,24 +13,24 @@
       <div class="video-container">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/_davN-ic8I0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-    <p> If you are experiencing any difiiculties with the above link, please <a target="_blank" href="https://www.youtube.com/embed/_davN-ic8I0">click here</a> to watch directly on Youtube.</p>
+    <p> If you are experiencing any difiiculties with the above link, please <a class="whitelink" target="_blank" href="https://www.youtube.com/embed/_davN-ic8I0">click here</a> to watch directly on Youtube.</p>
       <div>
       <h2>Church Service - Order - 12pm</h2>
       <ul class="fa-ul">
-        <li>Service starts at 12pm at Bromford Bridge Christian Centre</li>
-        <li>Entry music: Jesu Joy of Man's desire – recorded by Brian Atkins</li>
-        <li>Welcome &amp; Introduction – Matthew Atkins</li>
-        <li>Song:<a href="https://www.wearegateway.ca/it/downloads/music/lyrics/10,000Reasons.pdf" target=”_blank”> 10,000 Reasons (Bless the Lord)</a></li>
-        <li>Bible reading – Psalm 116:1-7</li>
-        <li>Tribute address - Brian Atkins</li>
-        <li>Hymn:<a href="https://library.timelesstruths.org/music/There_Is_Power_in_the_Blood/" target="_blank"> Would you be free (There is power)</a></li>
-        <li>Prayers</li>
-        <li>Tribute from Children</li>
-        <li>Song:<a href="https://district.church/wp-content/uploads/Way-Maker-Leeland-Lyrics.pdf" target="_blank"> Way maker</a></li>
-        <li>Bible reading – John 12: 24-26</li>
-        <li>Tributes from Grandchildren</li>
-        <li>Closing Prayer</li>
-        <li>Recessional music:<a href="https://www.wearegateway.ca/it/downloads/music/lyrics/NoLongerSlaves.pdf" target="_blank"> I am a Child of God</li>
+        <ListItem>Service starts at 12pm at Bromford Bridge Christian Centre</ListItem>
+        <ListItem>Entry music: Jesu Joy of Man's desire – recorded by Brian Atkins</ListItem>
+        <ListItem>Welcome &amp; Introduction – Matthew Atkins</ListItem>
+        <ListItem>Song:<a class="whitelink" href="https://www.wearegateway.ca/it/downloads/music/lyrics/10,000Reasons.pdf" target=”_blank”> 10,000 Reasons (Bless the Lord)</a></ListItem>
+        <ListItem>Bible reading – Psalm 116:1-7</ListItem>
+        <ListItem>Tribute address - Brian Atkins</ListItem>
+        <ListItem>Hymn:<a class="whitelink" href="https://library.timelesstruths.org/music/There_Is_Power_in_the_Blood/" target="_blank"> Would you be free (There is power)</a></ListItem>
+        <ListItem>Prayers</ListItem>
+        <ListItem>Tribute from Children</ListItem>
+        <ListItem>Song:<a class="whitelink" href="https://district.church/wp-content/uploads/Way-Maker-Leeland-Lyrics.pdf" target="_blank"> Way maker</a></ListItem>
+        <ListItem>Bible reading – John 12: 24-26</ListItem>
+        <ListItem>Tributes from Grandchildren</ListItem>
+        <ListItem>Closing Prayer</ListItem>
+        <ListItem>Recessional music:<a class="whitelink" href="https://www.wearegateway.ca/it/downloads/music/lyrics/NoLongerSlaves.pdf" target="_blank"> I am a Child of God</ListItem>
       </ul>
     </div>
     </div>
